051a4156364cee9f9fe47cd7e1ba2c222bf56cfa
Matt Stroud <lixingke06@gmail.com>
2020-06-05 14:23:58 -0700

    add AddTreat routes

--- SweetSavory/Controllers/TreatsController.cs
+++ SweetSavory/Controllers/TreatsController.cs
@@ -64,5 +64,23 @@ namespace SweetSavory.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index", "Home");
     }
+
+    public ActionResult AddTreat(int id)
+    {
+      Treat thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
+      return View(thisTreat);
+    }
+
+    [HttpPost]
+    public ActionResult AddTreat(Treat treat, int flavorId)
+    {
+      if (flavorId != 0)
+      {
+        _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = flavorId, TreatId = treat.TreatId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index", "Home");
+    }
   }
 }
