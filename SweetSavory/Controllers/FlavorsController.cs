bd368ce6d57402e0399fe53eee9a853b07b552eb
Matt Stroud <lixingke06@gmail.com>
2020-06-05 10:48:15 -0700

    add details route

--- SweetSavory/Controllers/FlavorsController.cs
+++ SweetSavory/Controllers/FlavorsController.cs
@@ -39,5 +39,15 @@ namespace SweetSavory.Controllers
       _db.SaveChanges();
       return View();
     }
+
+    public ActionResult Details(int id)
+    {
+      Flavor thisFlavor = _db.Flavors
+        .Include(flavor => flavor.Treats)
+        .ThenInclude(join => join.Treat)
+        .FirstOrDefault(flavor => flavor.FlavorId == id);
+
+      return View(thisFlavor);
+    }
   }
 }
