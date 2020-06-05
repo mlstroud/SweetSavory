2e8fa7970e0b3f912decdf78ae28984fa73b2e24
Matt Stroud <lixingke06@gmail.com>
2020-06-05 10:06:32 -0700

    add flavors and treats to viewbagg for index

--- SweetSavory/Controllers/HomeController.cs
+++ SweetSavory/Controllers/HomeController.cs
@@ -1,11 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using SweetSavory.Models;
 
 namespace SweetSavory.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly SweetSavoryContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public HomeController(UserManager<ApplicationUser> userManager, SweetSavoryContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+
     public ActionResult Index()
     {
+      ViewBag.Flavors = _db.Flavors.ToList();
+      ViewBag.Treats = _db.Treats.ToList();
       return View();
     }
   }
