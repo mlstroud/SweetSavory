a6595efdca43497669b4768580b285d8ef0ee361
Matt Stroud <lixingke06@gmail.com>
2020-06-05 15:20:16 -0700

    add route for DeleteTreat

--- SweetSavory/Controllers/FlavorsController.cs
+++ SweetSavory/Controllers/FlavorsController.cs
@@ -100,5 +100,14 @@ namespace SweetSavory.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index", "Home");
     }
+
+    [HttpPost]
+    public ActionResult DeleteTreat(int joinId)
+    {
+      var joinEntry = _db.FlavorTreat.FirstOrDefault(entry => entry.FlavorTreatId == joinId);
+      _db.FlavorTreat.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index", "Home");
+    }
   }
 }
